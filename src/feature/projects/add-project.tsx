64f19766e540c80d4ba29d78863f867409c867f7
MHuesler <hueslermaxim@gmail.com>
2022-05-05 00:23:30 +0200

    minor

--- src/feature/projects/add-project.tsx
+++ src/feature/projects/add-project.tsx
@@ -6,6 +6,12 @@ const AddProject = () => {
   const [active, setActive] = useState(0);
   const nextStep = () => setActive((current) => (current < 2 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
+  enum pricingTiers {
+    HOBBY,
+    GROWTH,
+    PROFESSIONAL
+  }
   
   return (
     <>
@@ -13,29 +19,25 @@ const AddProject = () => {
         <Stepper.Step label="Tier" description="Choose the project tier">
           <Grid>
             <Grid.Col span={6}>
-              <Paper flowy-feature={"addProject-2"} flowy-type="form" withBorder={true} radius={'md'} mb={'md'} p="md">
+              <Paper flowy-feature={"addProject-2"} onClick={nextStep} flowy-type="form" withBorder={true} radius={'md'} mb={'md'} p="md">
                 <Title order={5}>Growth</Title>
                 <Title order={2}>$49/mo</Title>
                 <Text>This tier is designed for startups. Please follow the <a>fair-use policy</a>.</Text>
               </Paper>
             </Grid.Col>
             <Grid.Col span={6}>
-              <Paper flowy-feature={"addProject-2"} flowy-type="form" withBorder={true} radius={'md'} mb={'md'} p="md">
+              <Paper flowy-feature={"addProject-2"} onClick={nextStep} flowy-type="form" withBorder={true} radius={'md'} mb={'md'} p="md">
                 <Title order={5}>Professional</Title>
                 <Title order={2}>$99/mo</Title>
                 <Text>This tier is designed for enterprises. Please follow the <a>fair-use policy</a>.</Text>
               </Paper>
             </Grid.Col>
           </Grid>
-          <Paper flowy-feature={"addProject-2"} flowy-type="form" withBorder={true} radius={'md'} mb={'md'} p="md">
+          <Paper flowy-feature={"addProject-2"} onClick={nextStep} flowy-type="form" withBorder={true} radius={'md'} mb={'md'} p="md">
             <Title order={5}>Hobby</Title>
             <Title order={2}>$4.99/mo</Title>
             <Text>This tier is designed for hobbyists. Please follow the <a>fair-use policy</a>.</Text>
           </Paper>
-          <Group position="center" mt="xl">
-            <Button variant="default" onClick={prevStep}>Back</Button>
-            <Button onClick={nextStep} flowy-feature={"addProject-3"}>Next step</Button>
-          </Group>
         </Stepper.Step>
         <Stepper.Step label="General" description="Internal information">
           <form flowy-feature={"addProject-4"} flowy-type="form">
