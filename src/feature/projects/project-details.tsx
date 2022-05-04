c72b10806d531b4cedc91449b467dd202d65b38e
MHuesler <hueslermaxim@gmail.com>
2022-05-05 00:56:13 +0200

    improvements details

--- src/feature/projects/project-details.tsx
+++ src/feature/projects/project-details.tsx
@@ -1,10 +1,62 @@
-import { Button, Title } from "@mantine/core";
+import { Box, Button, Grid, Group, Paper, SimpleGrid, Title, Text, createStyles, Badge } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    padding: theme.spacing.xl * 1.5,
+  },
+
+  label: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  },
+}));
 
 const ProjectDetailsComponent = () => {
+  const { classes } = useStyles();
+  
   return (
     <section>
       <Title>Project</Title>
-      <Button onClick={(() => { testGen(); })}>Generate docs</Button>
+      <Group position={'apart'} mb={"lg"}>
+        <Title order={2}>Therapyapp</Title>
+        <Box>
+          <Button onClick={(() => { testGen(); })} mr={'md'}>Generate docs</Button>
+          <Button>Visit Site</Button>
+        </Box>
+      </Group>
+      <Paper p={'sm'} radius={'md'} shadow={'sm'}>
+        <Grid>
+          <Grid.Col span={6}>
+
+          </Grid.Col>
+          <Grid.Col span={6} p={'md'}>
+            <Box mb={10}>
+              <Text
+                  color="dimmed"
+                  transform="uppercase"
+                  weight={700}
+                  size="xs"
+                  className={classes.label}
+                >
+                Domains
+              </Text>
+              <Text size="md" weight={600}>docs.therapyapp.ch</Text>
+              <Text size="md" weight={600}>therapyapp.flowyapps.com</Text>
+            </Box>
+            <Box>
+              <Text
+                  color="dimmed"
+                  transform="uppercase"
+                  weight={700}
+                  size="xs"
+                  className={classes.label}
+                >
+                Status
+              </Text>
+              <Badge variant={'dot'} color={'green'}>Running</Badge>
+            </Box>
+          </Grid.Col>
+        </Grid>
+      </Paper>
     </section>
   );
 }
